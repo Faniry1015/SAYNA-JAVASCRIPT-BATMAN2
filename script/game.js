fc1e6611b4caf809f51502c5eb64f6a2f70bbb36
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-06-25 08:09:14 +0300

    Add all code in async function main and fetch sayna API

--- script/game.js
+++ script/game.js
@@ -1,138 +1,128 @@
 async function main() {
-    const r = await fetch("")
-}
-
-const Q = [
-    {
-        question: "Quel est l’autre nom de l’Homme-Mystère ?",
-        response: [
-            {
-                text: "Le Saphinx",
-                isGood: true
-            },
-            {
-                text: "Le Saphir",
-                isGood: false
-            },gi
-            {
-                text: "Le Joker",
-                isGood: true
+    try {
+        const r = await fetch("https://batman-api.sayna.space/questions", {
+            headers: {
+                Accept: "application/json"
             }
-        ]
-    },
-    {
-        question: "Test 2 ?",
-        response: [
-            {
-                text: "Le Bobody",
-                isGood: false
-            },
-            {
-                text: "Le Bisou",
-                isGood: false
-            },
-            {
-                text: "Le Calin",
-                isGood: true
-            }
-        ]
-    },
-]
+        })
 
-//current questionnaire point(score)
-function score() {
-    const allCheckbox = document.querySelectorAll(".true")
-    allCheckbox.forEach((element) => {
-        if (element.className === "true") {
-            console.log("true")
+        if (!r.ok) {
+            throw new Error("Erreur de chargement du questionnaire depuis le serveur")
         }
-    })
-}
 
-//Choix pour chaque questionnaire courante
-function createResponse(choice) {
-    const createDivChoice = document.createElement("div")
-    createDivChoice.className = "choicesDiv"
-    createDivChoice.innerHTML =
-        `<input type="checkbox" class="checkB ${choice.isGood}">
+        const QBase = await r.json()
+
+        //Définition des variables
+        const totalQ = QBase.length
+        let curQId = 1
+        let curScore = 0
+        let questionIndex = 1
+
+        //Démarrer le QUIZZ
+        function showQ() {
+            const startGame = document.querySelector("#startGame")
+            const questionSect = document.querySelector("#questionnaire")
+            startGame.addEventListener("click", (e) => {
+                e.preventDefault
+                showQuestionnaire()
+
+                const currentQId = document.querySelector("#currentQId")
+                currentQId.innerText = curQId
+                createCurrentQuestionnaire(QBase[0])
+
+                const sec2 = document.querySelector("#sec2")
+                sec2.style.display = "none"
+            })
+
+            function showQuestionnaire() {
+                questionSect.style.display = "block"
+            }
+
+            const totQNbrSpan = document.querySelector("#totalQNbr")
+            totQNbrSpan.innerText = totalQ
+        }
+
+        showQ()
+
+        //Choix pour chaque questionnaire courante
+        function createResponse(choice) {
+            const createDivChoice = document.createElement("div")
+            createDivChoice.className = "choicesDiv"
+            createDivChoice.innerHTML =
+                `<input type="checkbox" class="checkB ${choice.isGood}">
         <label for="">${choice.text}</label>`
-    document.querySelector(".choisirReponse").append(createDivChoice)
-}
-
-//Current Questionnaire
-function createCurrentQuestionnaire(element) {
-    const currentQ = document.querySelector(".currentQuestion")
-    currentQ.innerText = element.question
-
-    for (let resp of element.response) {
-        createResponse(resp)
-    }
-
-}
-
-const totalQ = Q.length
-let curQId = 1
-let curScore = 0
-let questionIndex = 1
-
-function nextQ(database) {
-    const nextBtn = document.querySelector(".nextBtn")
-    nextBtn.addEventListener("click", () => {
-        //Check answer and add point
-        let answer = false
-        const allCheckbox = document.querySelectorAll(".checkB")
-        let allCheckboxTab = []
-        console.log(allCheckbox)
-        allCheckbox.forEach((element => {
-            if ((element.checked && element.classList.contains("true")) || (!element.checked && element.classList.contains("false"))) {
-                allCheckboxTab.push(true)
-            } else {
-                allCheckboxTab.push(false)
-            }
-        }))
-        console.log(allCheckboxTab)
-        answer = allCheckboxTab.every(element => element)
-        if (answer) {
-            ++curScore
+            document.querySelector(".choisirReponse").append(createDivChoice)
         }
-        console.log(curScore)
 
-        //Move to the next question
-        document.querySelectorAll(".choicesDiv").forEach(div => div.remove())
-        const currentChoice = document.querySelector(".choisirReponse div")
-        createCurrentQuestionnaire(Q[questionIndex])
-        questionIndex++
+        //Current Questionnaire
+        const currentQ = document.querySelector(".currentQuestion")
 
-        const totQNbrSpan = document.querySelector("#totalQNbr")
-        totQNbrSpan.innerText = totalQ
+        function createCurrentQuestionnaire(element) {
+            currentQ.innerText = element.question
+            for (let resp of element.response) {
+                createResponse(resp)
+            }
+        }
 
-        const currentQId = document.querySelector("#currentQId")
-        currentQId.innerText = ++curQId
-    })
+        //Check answer and add point
+        function checkAnswer() {
+            let answer = false
+            const allCheckbox = document.querySelectorAll(".checkB")
+            let allCheckboxTab = []
+            console.log(allCheckbox)
+            allCheckbox.forEach((element => {
+                if ((element.checked && element.classList.contains("true")) || (!element.checked && element.classList.contains("false"))) {
+                    allCheckboxTab.push(true)
+                } else {
+                    allCheckboxTab.push(false)
+                }
+            }))
+            console.log(allCheckboxTab)
+            answer = allCheckboxTab.every(element => element)
+            if (answer) {
+                ++curScore
+            }
+            console.log(curScore)
+        }
 
-}
+        function nextQ(database) {
+            const nextBtn = document.querySelector(".nextBtn")
+            nextBtn.addEventListener("click", () => {
+                checkAnswer()
+                //Move to the next question
+                document.querySelectorAll(".choicesDiv").forEach(div => div.remove())
 
-nextQ(Q)
+                currentQ.innerText = ""
+                createCurrentQuestionnaire(QBase[questionIndex])
+                questionIndex++
 
-//Démarrer le QUIZZ
-function showQ() {
-    const startGame = document.querySelector("#startGame")
-    const questionSect = document.querySelector("#questionnaire")
-    startGame.addEventListener("click", (e) => {
-        e.preventDefault
-        showQuestionnaire()
+                const totQNbrSpan = document.querySelector("#totalQNbr")
+                totQNbrSpan.innerText = totalQ
 
-        const currentQId = document.querySelector("#currentQId")
-        currentQId.innerText = curQId
-        createCurrentQuestionnaire(Q[0])
-    })
+                const currentQId = document.querySelector("#currentQId")
+                currentQId.innerText = ++curQId
+                console.log(curQId, totalQ)
 
-    function showQuestionnaire() {
-        questionSect.style.display = "block"
+                if (curQId === totalQ) {
+                    const questions = document.querySelector(".nextDiv")
+                    questions.style.display = "none"
+                    const showR = document.querySelector(".showDiv")
+                    showR.style.display = "block"
+                }
+            })
+
+        }
+        nextQ(QBase)
+
+        //Show Result
+        const showResult = document.querySelector(".showResult")
+        showResult.addEventListener("click", () => {
+            checkAnswer()
+        })
+
+    } catch (e) {
+        console.log("Impossible d'accèder au serveur", e)
     }
-
-    const totQNbrSpan = document.querySelector("#totalQNbr")
-    totQNbrSpan.innerText = totalQ
 }
 
-showQ()
+main()

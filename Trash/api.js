fad4cda7e86b68325349dede342e0ce5f9a1e470
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-06-26 09:18:52 +0300

    Remove all trash

--- Trash/api.js
+++
@@ -1,9 +0,0 @@
-export async function fetchJSON(url, options = {}) {
-    const headers = {Accept: "application/json", ...options.headers}
-    const r = await fetch(url, {...options, headers})
-    if (r.ok) {
-        return r.json()
-    } else {
-        throw new Error ("Erreur serveur", {cause: r})
-    }
-}
--- Trash/app.js
+++
@@ -1,3 +0,0 @@
-import { fetchJSON } from "./api.js";
-const questionaire = await fetchJSON("https://batman-api.sayna.space/questions")
-console.log(questionaire)

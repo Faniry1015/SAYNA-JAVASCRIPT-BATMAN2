663a0c0f6e417e6398427210432e80e3d342db15
Faniriantsoa RANDRIAHARIMINO <frandriaharimino@yahoo.com>
2023-07-04 20:44:07 +0300

    Logo batman qui suit la souris

--- script/main.js
+++ script/main.js
@@ -26,4 +26,18 @@ document.querySelectorAll(".reveal").forEach(function(r){
     observer.observe(r)
 })
 
+/**Image qui suit la souris */
+document.onmousemove = suitsouris;
+
+function suitsouris(evenement) {
+  if (navigator.appName == "Microsoft Internet Explorer") {
+    var x = event.x + document.body.scrollLeft;
+    var y = event.y + document.body.scrollTop;
+  } else {
+    var x = evenement.pageX;
+    var y = evenement.pageY;
+  }
+  document.getElementById("mouseImg").style.left = (x + 1) + 'px';
+  document.getElementById("mouseImg").style.top = (y + 1) + 'px';
+}
 
